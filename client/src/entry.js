089eb3e7fad9f582222ea3e5ee5d5c692dad73da
Jessica Tran Hur <jesshur824@gmail.com>
2016-02-18 20:12:25 -0800

    get ngmessages run for id validation

--- client/src/entry.js
+++ client/src/entry.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import filters from './filters';
+import ngMessages from 'angular-messages';
 
 
-var employeeApp = angular.module( 'employeeApp', []);
+var employeeApp = angular.module( 'employeeApp', ['ngMessages']);
 
 filters(employeeApp);
 
@@ -18,7 +19,6 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
 
   $http.get('https://openexchangerates.org/api/latest.json?app_id=fb4db514dcda4cce9452221d5993cc04')
        .then(res => {
-        console.log(res);
           $scope.currencies.JPY.rate = res.data.rates.JPY;
           $scope.currencies.CNY.rate = res.data.rates.CNY;
        });
@@ -28,7 +28,6 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
     $scope.employees = res.data;//Angular specific(?)
     $scope.employees.forEach(function(employee){
       employee.DOB = employee.DOB.substring(0,10);
-      employee.index = employee._id;
     });
   });
 
@@ -39,7 +38,6 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
          .then(
           function(res){
             $scope.employees.splice($scope.employees.indexOf(employee), 1);
-
             $scope.deleteConfirmation = 'Deleted Employee:';
             $scope.deletedEmployee = res.data;
             $scope.deletedEmployee.DOB = $scope.deletedEmployee.DOB.substring(0,10);
@@ -50,43 +48,33 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
   }
 
   //EDIT-PUT/PATCH
-  $scope.edit = function(employee) {
-    $scope.newEmployee = employee;
-    // $scope.idHolder = $scope.newEmployee._id;
+  $scope.edit = function(employee) { //when user clicks on the edit link next to the employee
+    $scope.newEmployee = angular.copy(employee);
+    $scope.employeeToEdit = employee;
     $scope.editEmployee = true;
+    $scope.disable = true;
   }
   $scope.cancelEdit = function() {
     $scope.editEmployee = null;
     $scope.badRequest = false;
-    $http.get('http://localhost:3000/api/employees/'+$scope.newEmployee.index)
-         .then(
-            function(res) {
-              $scope.employees.splice($scope.employees.indexOf($scope.newEmployee), 1);
-              res.data.index = res.data._id;
-              res.data.DOB = res.data.DOB.substring(0,10);
-              $scope.employees.push(res.data);
-              $scope.newEmployee = null;
-            },
-            function(err) {
-              $scope.badRequest = `${err.status}: ${err.data.errmsg}`;
-            }
-          );
+    $scope.newEmployee = null;
+    $scope.disable = false;
   }
   $scope.editSelectedEmployee = function() {
-    $http.put('http://localhost:3000/api/employees/'+$scope.newEmployee.index, $scope.newEmployee)
+    $http.put('http://localhost:3000/api/employees/'+$scope.newEmployee._id, $scope.newEmployee)
          .then(
             function(res){
-              $scope.employees.splice($scope.employees.indexOf($scope.newEmployee), 1);
-              res.data.index = res.data._id;
+              $scope.employees.splice($scope.employees.indexOf($scope.employeeToEdit), 1);
               res.data.DOB = res.data.DOB.substring(0,10);
               $scope.employees.push(res.data);
               $scope.newEmployee = null;
               $scope.editEmployee = null;
+              $scope.employeeToEdit = null;
               $scope.badRequest = false;
             },
             function(err){
               console.log(err);
-              $scope.badRequest = `${err.status}: ${err.data.errmsg}`;
+              $scope.badRequest = `${err.statusText}`;
               $scope.cancelEdit();
             }
           );
@@ -99,7 +87,6 @@ employeeApp.controller('EmployeeController', function($scope, $http) {
             function(res){
               var newEmployee = res.data;
               newEmployee.DOB = newEmployee.DOB.substring(0,10);
-              newEmployee.index = newEmployee._id;
               $scope.employees.push(newEmployee);
               $scope.badRequest = false;
               $scope.newEmployee = {};

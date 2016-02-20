64fccfa64b1815b641001955dfcd6a027b358855
Jessica Tran Hur <jesshur824@gmail.com>
2016-02-19 20:01:47 -0800

    omit isolated scope in this component. I don't want to change entry.js logic now

--- client/src/components/employee-edit/employee-edit.js
+++ client/src/components/employee-edit/employee-edit.js
@@ -6,14 +6,14 @@ export default function(AngularModule) {
       replace: true, //replace this element with content from this directive's html template
       restrict: 'E', //restrict this directive to be html tag element
       // transclude: true,//put content of parent html inside my tags
-      template,
+      template
       // scope:{
       //   label:'@', //only want to pass in value from outer scope to our "label" variable
       //   obj: '='
       // }
-      controller: ['$scope', function($scope) {
+      // controller: ['$scope', function($scope) {
 
-      }] //don't need to create controller for this component yet
+      // }] //don't need to create controller for this component yet
     }
   });
 };

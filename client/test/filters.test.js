0b702e3ffdbcf77be8606af638077ff1a25fbe23
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-02 15:02:01 -0800

    clean up filter test to make it more understandable

--- client/test/filters.test.js
+++ client/test/filters.test.js
@@ -1,7 +1,7 @@
 
 describe('filters', function() {
 
-  beforeEach(module('filters')); //same as using angular.mock.module
+  beforeEach(module('filters')); //same as using angular.mock.module. Have to roll this up because moneyExchange is not directly connected to any registered angular module
 
   describe('money filter', function() {
 
@@ -15,11 +15,18 @@ describe('filters', function() {
       rate: 1
     };
 
-    it('should convert boolean values to unicode checkmark or cross',
-      inject(function(moneyExchangeFilter) {
-        assert.equal(moneyExchangeFilter(1000, format1),'¥ 500 ');
-        assert.equal(moneyExchangeFilter(1000, format2),'$ 1k');
-        assert.equal(moneyExchangeFilter(1000000, format2),'$ 1m');
-      }));
+    var moneyExchangeFilter;
+
+    beforeEach(inject(_$filter_ => {
+      var $filter = _$filter_;
+      moneyExchangeFilter = $filter('moneyExchange'); //Use angular $filter service to instantiate an instance for moneyExchange
+    }));
+
+    it('should convert boolean values to unicode checkmark or cross', () => {
+      assert.equal(moneyExchangeFilter(1000, format1), '¥ 500 ');
+      assert.equal(moneyExchangeFilter(1000, format2), '$ 1k');
+      assert.equal(moneyExchangeFilter(1000000, format2), '$ 1m');
   });
+});
+
 });
